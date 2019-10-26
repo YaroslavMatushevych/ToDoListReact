e7cfd20aa641a7d4bebef5910b17a07d6b31aa38
yaroslav <optima19977@gmail.com>
2019-10-26 11:10:54 +0300

    Updates

---
+++ precache-manifest.65a88d13361209c8008b1217637d1b00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aae6cc8be45a39ad6fe8c749a495b67",
+    "url": "/ToDoListReact/index.html"
+  },
+  {
+    "revision": "9333eed13e115af58831",
+    "url": "/ToDoListReact/static/css/2.cc92112e.chunk.css"
+  },
+  {
+    "revision": "2f7e26a895c77621e5e3",
+    "url": "/ToDoListReact/static/css/main.acf9e9e4.chunk.css"
+  },
+  {
+    "revision": "9333eed13e115af58831",
+    "url": "/ToDoListReact/static/js/2.75265d1f.chunk.js"
+  },
+  {
+    "revision": "2f7e26a895c77621e5e3",
+    "url": "/ToDoListReact/static/js/main.09ef337c.chunk.js"
+  },
+  {
+    "revision": "5527cfa09a7d80dfab75",
+    "url": "/ToDoListReact/static/js/runtime-main.52a87e32.js"
+  },
+  {
+    "revision": "966603e765173d06924dc43920d8a243",
+    "url": "/ToDoListReact/static/media/YARMA.966603e7.png"
+  },
+  {
+    "revision": "a7da770a55e1464937aa592ad784e667",
+    "url": "/ToDoListReact/static/media/logo-text.a7da770a.png"
+  }
+]);
