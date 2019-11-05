f7e697c499ab4f48634b5a7fc34b3d21f711bb0c
yaroslav <optima19977@gmail.com>
2019-11-05 21:55:57 +0200

    comma delete

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TimerDeadline from '../TimerDeadline/TimerDeadline';
 import MyContext from '../../MyContext/MyContext';
